c1aaaf42e846ddf4ff2e76a26577f460e77ab8f8
Treasure Ojiya <ansgarojiya@gmail.com>
2026-03-12 22:49:35 +0100

    api-fix-evn-prod

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
 
-  baseUrl: 'http://51.89.226.178:5000/api/v1',
+  // baseUrl: 'http://51.89.226.178:5000/api/v1',
+
+  baseUrl: 'https://fin.aystar.space/api/v1',
 };
